72fb0db3cf2f10f719d0a5dd85817532e5b11ed5
Cory McDonald <cmcdonald@brave.com>
2020-03-30 16:16:14 -0500

    Rename ID to be SKU

--- src/Sku.php
+++ src/Sku.php
@@ -6,14 +6,14 @@ use Macaroons\Macaroon;
 use Macaroons\Packet;
 
 class Sku {
-    public static function generateSKUToken($secret, $id, $amount, $currency, $description, $expiry) {
+    public static function generateSKUToken($secret, $sku, $amount, $currency, $description, $expiry) {
        $identifier = "Brave SKU v1.0";
        // Current website or variable representing the website
        $location = $_SERVER['HTTP_HOST'];
 
        $m = new Macaroon($secret, $identifier, $location);
-  
-       $m->addFirstPartyCaveat('id = ' . $id);
+
+       $m->addFirstPartyCaveat('sku = ' . $sku);
        $m->addFirstPartyCaveat('amount = ' . $amount);
        $m->addFirstPartyCaveat('currency = ' . $currency);
        $m->addFirstPartyCaveat('description = ' . $description);
